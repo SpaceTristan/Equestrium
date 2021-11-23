623b1e334e0aa7ec37293105a67301e9fd2013f1
matthew.gonzalez <matthewg49@gmail.com>
2021-11-23 00:39:01 -0500

    Entering marble race

--- Contracts/EnterRace.sol
+++ Contracts/EnterRace.sol
@@ -1,17 +1,41 @@
 pragma solidity ^0.8.0;
 
+contract Race {
 
-contract EnterRace {
+    mapping(uint256 => uint256) public prize_pool;
+
+
+    //Place Bet function
+    function placeBet(uint256 betSize, uint256 marbleID, uint256 raceID) public external {
+        require(betSize in (1, 0.1, 0.01), "Bet sizes can only be 1, 0.1, 0.01.")
+        require(ownerOf(marbleID) == msg.sender, "This isn't your marble.")
+        require(raceID exists, "This race doesn't exist. Please try another.")
+        require(stakes(raceID) == betSize, "These aren't the correct stakes. Please select another race.")
+
+        prize_pool[raceID] += betSize;
+
+
+        //user places minimum bet of .01 ether (select from list of options )
+        //user owns the marble they are placing the bet on
+    }
+
     //Enter Functions
-    function enterHorse() private {
-        //Function will enter horse
-        //Entry FEe
+    function enterMarble() private {
+        //Function will enter marble
+        //charge fee that goes to the dApp
+        //raceID
 
     }
 
-    function raceWin() private {
-        //Distribute the winnings 
-        
+    // function raceWin() private {
+    //     //Distribute the winnings
+
+    // }
+
+    function finishRace() onlyOwner { //or authorized smart contract aka chainlink vrf is allowed to call this function
+        Select winner
+        release funds in pool variable to winner
+        reset pool number and funds
     }
 
 }
